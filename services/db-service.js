22c9d2958bffd48435fc21d0dd8319e8e95723d4
Ste11front <137266358+Ste11front@users.noreply.github.com>
2023-07-28 12:32:09 +0200

    Stato pubblicazione

--- services/db-service.js
+++ services/db-service.js
@@ -4,7 +4,7 @@ export class DBService{
 
     //GET
     static getAllTodos(){
-        const url = 'https://64b51348f3dbab5a95c6a622.mockapi.io/todos';
+        const url = 'https://64b512c1f3dbab5a95c6a48c.mockapi.io/todos';
         return fetch(url)
         .then(resp => resp.json())
         .then(result => this.convertToTodoArray(result))
@@ -14,14 +14,14 @@ export class DBService{
     //DELETE
     static deleteTodo(id){
         console.log('delete', id);
-        const deleteUrl = 'https://64b51348f3dbab5a95c6a622.mockapi.io/todos' + id;
+        const deleteUrl = 'https://64b512c1f3dbab5a95c6a48c.mockapi.io/todos' + id;
         console.log(deleteUrl);
         return fetch(deleteUrl, {method: 'delete'}).then(resp => resp.json());
     }
 
     //PUT
     static updateTodo(todo){
-        const updateUrl = 'https://64b51348f3dbab5a95c6a622.mockapi.io/todos' + todo.id;
+        const updateUrl = 'https://64b512c1f3dbab5a95c6a48c.mockapi.io/todos' + todo.id;
         return fetch(updateUrl, {method: 'put', 
                                  body: JSON.stringify(todo),
                                  headers: {
@@ -33,7 +33,7 @@ export class DBService{
 
     //UPDATE
     static saveTodo(todo){
-        const postUrl = 'https://64b51348f3dbab5a95c6a622.mockapi.io/todos'
+        const postUrl = 'https://64b512c1f3dbab5a95c6a48c.mockapi.io/todos'
         return fetch(postUrl, {method: 'post', 
                                  body: JSON.stringify(todo),
                                  headers: {
